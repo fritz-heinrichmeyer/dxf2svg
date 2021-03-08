fbae89e8124313fd9b387fa0c0983763ad9d3578
Fritz Heinrichmeyer <fritz.heinrichmeyer@gmail.com>
2021-03-08 18:49:15 +0100

    import math

--- tests/testspdb.py
+++ tests/testspdb.py
@@ -13,11 +13,12 @@
 # from testspdb import *
 from svgwrite.path import Path
 import svgwrite
-p= Path(d="M 0 0")
-p.push_arc(target=(7,7), rotation=30, r=(2,4), large_arc=False, angle_dir='-', absolute=True)
+import math
+p= Path(d=f"M {10 * math.sin(math.pi * 0.20)} {10 * math.sin(math.pi * 0.30)} ")
+p.push_arc(target=(10* -math.sin(math.pi * 0.20),10 *math.sin(math.pi * 0.30)), rotation=30, r=(10,10), large_arc=False, angle_dir='-', absolute=True)
 
 
-svg_entity = svgwrite.Drawing().path(d=p.commands,stroke="blue", stroke_width="5")
+svg_entity = svgwrite.Drawing().path(d=p.commands,stroke="blue", stroke_width="0.01")
 print (svg_entity._repr_svg_())
 print (svg_entity.commands)
 print (p.commands)
