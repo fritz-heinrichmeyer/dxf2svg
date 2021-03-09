76639256ed7a4a9b3243ff54dafdfe4837f6dae5
Fritz Heinrichmeyer <fritz.heinrichmeyer@gmail.com>
2021-03-09 15:29:38 +0100

    center, arc_start, arc_end

--- tests/testspdb.py
+++ tests/testspdb.py
@@ -14,8 +14,12 @@
 from svgwrite.path import Path
 import svgwrite
 import math
-p= Path(d=f"M {10 * math.sin(math.pi * 0.20)} {10 * math.sin(math.pi * 0.30)} ")
-p.push_arc(target=(10* -math.sin(math.pi * 0.20),10 *math.sin(math.pi * 0.30)), rotation=30, r=(10,10), large_arc=False, angle_dir='-', absolute=True)
+center=(10,10)
+arc_start= 30
+arc_end= 150
+p= Path(d=f"M {10 * math.cos(math.pi *arc_start/180 ) +center[0] } {10 * math.sin(math.pi * arc_start/180) +center[1]} ")
+target=(10 * math.cos(math.pi *arc_end/180 ) +center[0], 10 * math.sin(math.pi * arc_end/180)+center[1])
+p.push_arc(target=(10 * math.cos(math.pi *arc_end/180 ) +center[0], 10 * math.sin(math.pi * arc_end/180)+center[1]), rotation=30, r=center, large_arc=True, angle_dir='-', absolute=True)
 
 
 svg_entity = svgwrite.Drawing().path(d=p.commands,stroke="blue", stroke_width="0.01")
